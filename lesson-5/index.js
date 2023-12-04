7a295d3b5e51601de9e6d93b4b1e591b4a0d6641
baibars313 <baibarsrajput@gmail.com>
2023-12-04 13:52:22 +0500

    added

---
+++ lesson-5/index.js
@@ -0,0 +1,16 @@
+// document.getElementsByTagName("p")[1].innerText="lorem ojoi hjoij poij pookjpoikkpo iupo 9iu"
+
+// document.getElementById("p1").innerText="12335677"
+
+var a=10;
+var arr= [ "Imran" , "iftikhar" , "asghar", "shahid",8,a]
+
+// var pTags=document.getElementsByTagName("p")
+// console.log(pTags)
+// pTags[0].innerText=arr[0]
+// pTags[1].innerText=arr[1]
+// pTags[2].innerText=arr[2]
+// pTags[1].innerText=arr[4]
+
+var gs=document.getElementsByClassName("text-g")
+gs[0].innerText="h1 tag"
