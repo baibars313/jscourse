ecec80f9dfffaba994587753dd061e22cc3f4351
baibars313 <baibarsrajput@gmail.com>
2023-12-11 15:18:34 +0500

    added

---
+++ lesson-6/index.js
@@ -0,0 +1,35 @@
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+//     if ( i==10) {
+//         break;
+//     }
+//   }
+  
+  // var j = 1;
+
+//   console.log(j++); // This will log the current value of j (1) and then increment it
+  
+//   // Now, if you log j again, you'll see the incremented value
+//   console.log(j);
+  // let i = 0;
+//   while (i<10) {
+//     console.log(i);
+//     i++;
+//   }
+  
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
+
+// let person = { name: 'John', age: 30, job: 'developer' };
+// for (let key in person) {
+//   console.log(key + ': ' + person[key]);
+// }
+
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
