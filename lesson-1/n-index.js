30c2dbf56bcb0f8a4b9cfc86de66bfcd6943b1dd
baibars313 <baibarsrajput@gmail.com>
2024-01-23 13:32:22 +0500

    added

---
+++ lesson-1/n-index.js
@@ -0,0 +1,19 @@
+// console.log("hello world!")
+
+var a=30
+let b=3
+let c="hellow world"
+// const f="hello world"
+
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log(a-b)
+ var a=10
+ b=10
+
+ 
+ 
+// console.log(a,b,f)
+let f=10;
+console.log(c,f)
