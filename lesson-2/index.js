a3fd2413c514cd72cef4d52ad2ec7d30e4e6fef1
baibars313 <baibarsrajput@gmail.com>
2024-01-25 14:17:57 +0500

    added

--- lesson-2/index.js
+++ lesson-2/index.js
@@ -6,17 +6,9 @@ console.log(a1 + c,"plus")
 //var a1=20
 console.log(a1 + c,"plus")
 var b3 = 20
-console.log(c - a1,"minus")
+console.log(c - a1,"nasir")
 console.log(c / a1,"division")
 console.log(c*a1,"multiplication")
-console.log(c===a1)
-if (a1===c && a1===b3) {
-      console.log(c - a1,"minus")  
-}
-else if (a1===b || a1 ===c3 ){
 
-      console.log(c * a1,"mult")  
-}
-else{
-      console.log(c + a1,"minus")    
-}
+let strs='1bcokp oqkerpo RPO34Kiprotiko4tik4pwotkipow4t ki 4powtw4apot5w4kapotik4pwoa4ow'
+console.log(strs,a1-c)
